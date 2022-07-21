7290946ba3ed5ded76cf0300f6f4af2f4675485b
DuyViet-Vo <duyviet1810@gmail.com>
2022-07-21 11:03:36 +0700

    CV cua Viet

---
+++ cv.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        name:'vo duy viet',
+        job:'intern front end (vuejs)',
+        work:'No',
+        activate: 'No',
+        date:'18/10/2001',
+        phone:'0392252429',
+        email:'duyviet1810@gmail.com',
+        
+    },
+    methods :{
+
+    },
+})
